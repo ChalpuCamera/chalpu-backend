f8a5447cf87275028808f5e807947d0a5fb31f49
GoToBILL <wnqudwnrlrl@naver.com>
2025-07-22 17:05:36 +0900

    refactor: tip 수정

--- src/main/java/com/example/chalpu/home/dto/PhotoTipDto.java
+++ src/main/java/com/example/chalpu/home/dto/PhotoTipDto.java
@@ -5,5 +5,5 @@ import com.example.chalpu.home.domain.PhotoTip;
 public record PhotoTipDto(String id, String title, String text) {
     public static PhotoTipDto from(PhotoTip tip) {
         return new PhotoTipDto(tip.getId(), tip.getTitle(), tip.getText());
-    }
+    }   
 } 
