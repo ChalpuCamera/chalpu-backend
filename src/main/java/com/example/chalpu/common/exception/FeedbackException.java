af65dc344b1de1776ca0d67da7bb161b17a3b5c1
GoToBILL <wnqudwnrlrl@naver.com>
2025-08-11 14:11:11 +0900

    refactor: Move domain-specific exceptions to common package
    - Move RewardException from reward.exception to common.exception
    - Move SurveyException from survey.exception to common.exception
    - Move FeedbackException from customerfeedback.exception to common.exception
    - Move CustomerException to common.exception (already done)
    - Centralize all custom exceptions for better organization and reusability

---
+++ src/main/java/com/example/chalpu/common/exception/FeedbackException.java
@@ -0,0 +1,8 @@
+package com.example.chalpu.common.exception;
+
+public class FeedbackException extends BaseException {
+
+    public FeedbackException(ErrorMessage errorMessage) {
+        super(errorMessage);
+    }
+}
